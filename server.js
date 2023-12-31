970d454648012382e1cd0293bbfe457fa6f7e8bd
krosengr4 <rosenkev4@gmail.com>
2023-12-31 01:53:58 -0600

    code server.js file

---
+++ server.js
@@ -0,0 +1,23 @@
+// import express middlewear
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// import and initialize our routes
+const routes = require('./routes');
+app.use(routes);
+
+// import database connection to MongoDB
+const mongoDB = require('./config/connection');
+
+// create local host port
+const PORT = process.env.PORT || 3003;
+
+// start server. .once() listens to open event once (= suc. mongoDB connection)
+mongoDB.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server is running at: http://localhost:${PORT}`);
+    });
+});
+
