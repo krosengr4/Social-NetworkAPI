06ce078852f0f78809e7be79c38d15a01d95545e
krosengr4 <rosenkev4@gmail.com>
2024-01-01 15:02:20 -0600

    Code controlles to get all users and specific user

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,8 +7,40 @@ const { User, Thought } = require('../models');
 module.exports = {
 
     // Get all users
+    async getUsers(req, res) {
+        try {
+            const users = await User.find();
+
+            const userObj = {
+                users,
+                headCount: await headCount(),
+            };
+
+            res.json(userObj);
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
     // Get a single user
+    async getSingleUser(req, res) {
+        try {
+            const user = await User.findOne({ _id: req.params.userId })
+                .select('-__v');
+
+            if (!user) {
+                return res.status(404).json({ message: "No user with that ID"})
+            }
+
+            res.json({
+                user,
+            });
+        } catch (err) {
+            console.log(err);
+            return res.status(500).json(err);
+        }
+    },
 
     // Create new user
 
