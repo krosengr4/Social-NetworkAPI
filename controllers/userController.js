00b1c4b8a7c567bbe6721ae1ef3fe3ad941e4142
krosengr4 <rosenkev4@gmail.com>
2024-01-01 07:20:25 -0600

    pseudo code what do in user controler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,23 @@
 //! This file contains controllers to get, create, update, and delete a user, as well as add and remove a friend.
+
+// import User and Thought model
+const { User, Thought } = require('../models');
+
+//export all get, create, update, and delete users && add and remove friends
+module.exports = {
+
+    // Get all users
+
+    // Get a single user
+
+    // Create new user
+
+    // Update a user
+
+    // Delete a user
+
+    // add a friend to a user
+
+    // remove a friend from a user
+
+};
