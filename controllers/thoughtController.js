aa004d3e5ae819083790ee9758266b1f8c7a9e0b
krosengr4 <rosenkev4@gmail.com>
2024-01-02 01:30:47 -0600

    Code in thoughtController make controllers to get all, get one, create and delete thoughts

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -7,14 +7,52 @@ const { User, Thought } = require('../models');
 module.exports = {
 
     // Get all thoughts
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find();
+            res.json(thoughts);
+        } catch(err) {
+            res.status(500).json(err);
+        };
+    },
 
     // Get a single thought
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId});
+
+            if(!thought) {
+            return res.status(404).json({ message: 'No thought with that ID' });
+            };
+            
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 
     // Create a thought
-
-    // Update a thought
+    async createThought(req, res) {
+        try {
+            const thought = await Thought.create(req.body);
+            res.json(thought);
+        } catch (err) {
+            res.status(505).json(err);
+        }
+    },
 
     // Delete a thought
+    async deleteThought(req, res) {
+        try {
+            const thought = await Thought.findOneAndDelete({ _id: req.params.thoughtId });
+            
+            res.status(200).json({ message: `Thought successfully deleted`})
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    }
+    
+    // Update a thought
 
     // Create reaction on specific thought
 
